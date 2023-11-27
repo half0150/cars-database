04a78991974263740cbe060376611ed1e6c7710a
Halfdan Karlsen Monie <half0150@edu.sde.dk>
2023-11-27 11:19:45 +0100

    netbeans update

--- cars.php
+++ cars.php
@@ -5,26 +5,35 @@ function isLicenseValid($license) {
     return preg_match("/^[A-Za-z]{2}[0-9]{5}$/", $license);
 }
 
+$showMessage = false;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $brand = $_POST['brand'];
     $model = $_POST['model'];
     $license = $_POST['license'];
+    $year = $_POST['year'];
+    $vinno = $_POST['vinno'];
 
-    if (isLicenseValid($license)) {
-        $stmt = $conn->prepare("INSERT INTO cars (brand, model, license) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $brand, $model, $license);
+    if (!empty($brand) || !empty($model) || !empty($license) || !empty($year) || !empty($vinno)) {
+        $showMessage = true;
 
-        if ($stmt->execute()) {
-            echo "Car has been registered";
+        if (isLicenseValid($license)) {
+            $stmt = $conn->prepare("INSERT INTO cars (brand, model, year, license, vinno) VALUES (?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssss", $brand, $model, $year, $license, $vinno);
+
+            if ($stmt->execute()) {
+                $message = "Car has been registered";
+            } else {
+                $message = "Error: " . $stmt->error;
+            }
+
+            $stmt->close();
         } else {
-            echo "Error: " . $stmt->error;
+            $message = "Invalid license format";
         }
-
-        $stmt->close();
-    } else {
-        echo "Invalid license format";
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -38,10 +47,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <form action="cars.php" method="post">
         <input type="text" name="brand" placeholder="brand">
         <input type="text" name="model" placeholder="model">
+        <input type="number" name="year" placeholder="1999">
         <input type="text" name="license" placeholder="AA12345">
+        <input type="text" name="vinno" placeholder="VIN">
         <button type="submit">Register</button>
     </form>
-    
+
+    <?php
+    if ($showMessage) {
+        echo "<p>{$message}</p>";
+    }
+    ?>
+
     <button onclick="location.href='garage.php'">View Registered Cars</button>
 </body>
 </html>
